4a3122902d847ef3d5a49ef0f65064f0339545ff
Sergei Sharaburko <s.sharaburko@yandex.ru>
2024-01-18 11:27:14 +0300

    newEdition180124
    errorPush

--- move.h
+++ move.h
@@ -6,18 +6,3 @@
 std::vector <std::vector <int>> movesActivChip(int activChip, std::vector <std::vector <int>> arr);
 
 
-//std::vector <std::vector <int>> arr = {
-//	{1,4},
-//	{2,5},
-//	{3,6},
-//	{4,5},
-//	{5,6},
-//	{4,7},
-//	{5,8},
-//	{6,9}
-//};
-
-
-
-
-
