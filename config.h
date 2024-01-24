63a01b755a4779260154584fa2bd002a53c30b12
Sergei Sharaburko <s.sharaburko@yandex.ru>
2024-01-24 14:12:02 +0300

    err

--- config.h
+++ config.h
@@ -4,133 +4,41 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-float radiusChip = 15;
-sf::Vector2f sizePoints(40, 40);
-
 class Config {
-    struct Coordinate { //структура для хранения координат точек
+    int chipCount = 0;
+    int pointsCount = 0;
+    int connectCount = 0;
+    struct Coordinate;
+    struct ConnectionsBetweenPoints;
+    std::vector <Coordinate> points;
+    std::vector <int> arrStartPoints;
+    std::vector <int> arrWinnerPoints;
+    std::vector <ConnectionsBetweenPoints> connection;
 
+    struct Coordinate { 
         Coordinate() { x = y = 0; }
         Coordinate(const float x, const float y) : x(x), y(y) {}
         float getCoordinateX() const { return x; }
         float getCoordinateY() const { return y; }
     private:
         float x, y;
-    };
-
-    int chipCount = 0;                 //количество фишек
-    int pointsCount = 0;               //количество точек
-    vector <Coordinate> points;        //вектор с координатами точек
-    vector <int> arrStartPoints;       //стартовые позиции фишек
-    vector <int> arrWinnerPoints;      //победные позиции фишек
-    int connectCount = 0;              //количество соединений
-
-
-    struct ConnectionsBetweenPoints { //структура для хранения соединенных точек
+    };          
+    
+    struct ConnectionsBetweenPoints { 
         ConnectionsBetweenPoints(const int p1, const int p2) : p1(p1), p2(p2) {}
         int getConnectionP1() const { return p1; }
         int getConnectionP2() const { return p2; }
      private:
-        int p1, p2; // параметры сединеных точек
+        int p1, p2; 
     };
 
-    vector <ConnectionsBetweenPoints> connection;
 public:
-    inline void readConfig(string const configFilePath);
-    Coordinate getCoordinatePoints(int pointNumber) { return points[pointNumber - 1]; }
+    void readConfig(std::string const configFilePath);    
     int getPointsCount() const {return pointsCount;}
     int getChipCount() const { return chipCount; }
     int getArrStartPoints(int i) { return arrStartPoints[i]; }
     int getArrWinnerPoints(int i) { return arrWinnerPoints[i]; }
     int getConnectCount() const { return connectCount; }
+    Coordinate getCoordinatePoints(int pointNumber) { return points[pointNumber - 1]; }
     ConnectionsBetweenPoints getConnectionsBetweenPoints(int connectCount) { return connection[connectCount]; }
-};
- 
-    //СЧИТЫВАНИЕ ВХОДНЫХ ДАННЫХ
-void Config::readConfig(string  const configFilePath) {
-    ifstream cfg;
-    cfg.open(configFilePath);
-
-    if (!cfg.is_open()) {
-        cout << "Configuration file not found!\n";
-        return;
-    }
-
-    cfg >> chipCount;	
-    cfg >> pointsCount;
-    points.reserve(pointsCount);
-    arrStartPoints.reserve(chipCount);
-    arrWinnerPoints.reserve(chipCount);
-
-    for (int i = 0; i < pointsCount; i++) {
-        int x, y;
-        string str;
-        cfg >> str;
-
-        if (!str.empty()) {
-            int positionComma = str.find(',');
-            x = atoi(str.c_str());
-            str.erase(0, positionComma + 1);
-            y = atoi(str.c_str());
-            Coordinate coordTemp(x, y);
-            points.push_back(coordTemp);
-        }
-
-    }
-
-    for (int i = 0; i < chipCount; i++) {
-	    string str;
-        int temp;
-
-        if (i < chipCount - 1) {
-            getline(cfg, str, ',');
-        }
-        else
-        {
-            cfg >> str;
-        }
-
-        temp = atoi(str.c_str());
-        arrStartPoints.push_back(temp);
-    }
-
-    for (int i = 0; i < chipCount; i++) {
-        string str;
-        int temp;
-        if (i < chipCount - 1) {
-            getline(cfg, str, ',');
-        }
-        else
-        {
-            cfg >> str;
-        }
-        temp = atoi(str.c_str());
-        arrWinnerPoints.push_back(temp);
-    }
-
-    cfg >> connectCount;
-    connection.reserve(connectCount);
-
-    for (int i = 0; i < connectCount; i++) {
-        int p1, p2;
-        string str;
-        cfg >> str;
-
-        if (!str.empty()) {
-            int positionComma = str.find(',');
-            p1 = atoi(str.c_str());
-            str.erase(0, positionComma + 1);
-            p2 = atoi(str.c_str());
-            ConnectionsBetweenPoints connPoint(p1, p2);
-            connection.push_back(connPoint);
-        }
-
-    }
-    cfg.close();
-}
-	
-
-
-
+}; 
--- main.cpp
+++ main.cpp
@@ -5,11 +5,9 @@
 #include "config.h"
 #include "move.h"
 
-using namespace sf;
-vector <sf::Color> arrColor{Color::Black, sf::Color::White, Color::Red, Color::Green, Color::Blue, Color::Magenta, Color::Cyan, Color::Transparent };    //color points and chip
-
-//Color yellow(sf::Color::Yellow);
-
+std::vector <sf::Color> arrColor{sf::Color::Black, sf::Color::White, sf::Color::Green, sf::Color::Blue, sf::Color::Magenta, userColor::Purple, userColor::Olive, userColor::Gray, userColor::Navy, userColor::Fuchsia, userColor::Teal};    //color points and chip
+float radiusChip = 15;
+sf::Vector2f sizePoints(40, 40);
 struct Chip {
     CircleShape shape;
     int numberPositionShape;
@@ -47,10 +45,8 @@ struct PositionPoints {
     }
 };
 
-
-int main()
-{ 
-    RenderWindow window(VideoMode(640, 480), "Game");
+int main() { 
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Game_Sharaburko", sf::Style::Close);
     Config config;
     config.readConfig("config.txt");
     sf::Mouse mouse;
@@ -65,11 +61,9 @@ int main()
     int activChip = 0;
     int stepActivChip = 0;
     int countWinPosition = 0;
-
-    vector <Chip> chip;   
-    vector <Square> square;
-    vector <PositionPoints> positionPoints;
-
+    std::vector <Chip> chip;   
+    std::vector <Square> square;
+    std::vector <PositionPoints> positionPoints;
     chip.reserve(config.getChipCount());
     square.reserve(config.getChipCount());
     positionPoints.reserve(config.getPointsCount());
@@ -79,9 +73,8 @@ int main()
     std::vector <int> roadActivChip;
     std::vector <std::vector <int>> road;
     connectPoints.reserve(config.getConnectCount());
-
-    for (int i = 0; i < config.getConnectCount(); i++)
-    {
+    
+    for (int i = 0; i < config.getConnectCount(); i++) {
         std::vector <int> tempConnect;
         tempConnect.push_back(config.getConnectionsBetweenPoints(i).getConnectionP1());
         tempConnect.push_back(config.getConnectionsBetweenPoints(i).getConnectionP2());
@@ -105,13 +98,12 @@ int main()
         PositionPoints tempPositionPoints(i, config.getCoordinatePoints(i).getCoordinateX(), config.getCoordinatePoints(i).getCoordinateY());
         positionPoints.push_back(tempPositionPoints);
     }
-	
-    while (window.isOpen())
-    {
+    	
+    while (window.isOpen()) {
         sf::Event event;
     	
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
@@ -130,8 +122,7 @@ int main()
             occupPoints.push_back(chip[i].numberPositionShape);
         }
 
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-        {
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
             mousePosition = mouse.getPosition(window);
 
             for (int i = 0; i < positionPoints.size(); i++) {
@@ -173,14 +164,11 @@ int main()
 
         road = movesActivChip(activChip, connectPoints);
 
-        for (size_t j = 0; j < road.size(); j++)
-        {
+        for (size_t j = 0; j < road.size(); j++) {
 
-            for (size_t k = 0; k < occupPoints.size(); k++)
-            {
+            for (size_t k = 0; k < occupPoints.size(); k++) {
 
-                if (find(road[j].begin() + 1, road[j].end(), occupPoints[k]) != road[j].end())
-                {
+                if (find(road[j].begin() + 1, road[j].end(), occupPoints[k]) != road[j].end()) {
                     road.erase(road.begin() + j);
                     j--;
                     break;
@@ -189,15 +177,9 @@ int main()
             }
         }
 
-        //if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
-        //{
-        //    for (size_t z = 0; z < road.size(); z++)
-        //    {
-        //        for (size_t l = 0; l < road[z].size(); l++)
-        //        {
-        //            std::cout << road[z][l] << " ";
-        //        }
-        //        std::cout << "\n";
+        if (countWinPosition == chip.size()) 
+            break;
+        
 
         //    }
         //}
@@ -208,66 +190,58 @@ int main()
             window.clear(sf::Color(214, 203, 174));
             countWinPosition = 0;
 
-            for (size_t i = 0; i < road.size(); i++)
-            {
-            sf:CircleShape activ;
-                activ.setFillColor(sf::Color(214, 203, 174));
-                activ.setRadius(radiusChip * 1.5);
-                activ.setOutlineThickness(2);
-                activ.setOutlineColor(sf::Color::Red);
-                activ.setPosition(positionPoints[*(road[i].end() - 1) - 1].coordinateX, positionPoints[*(road[i].end() - 1) - 1].coordinateY);
+        for (int i = 0; i < config.getConnectCount(); i++) {
+            int p1 = config.getConnectionsBetweenPoints(i).getConnectionP1();
+            int p2 = config.getConnectionsBetweenPoints(i).getConnectionP2();
+            float p1X = config.getCoordinatePoints(p1).getCoordinateX();
+            float p1Y = config.getCoordinatePoints(p1).getCoordinateY();
+            float p2X = config.getCoordinatePoints(p2).getCoordinateX();
+            float p2Y = config.getCoordinatePoints(p2).getCoordinateY();
+            float widthConnection = 20;
+            sf::RectangleShape connectingPoints;
 
-                window.draw(activ);
+            if (p1X == p2X) {
+                sf::Vector2f size(widthConnection, p2Y + sizePoints.y - p1Y - (sizePoints.y - widthConnection));
+                sf::Vector2f position(p1X + (2 * radiusChip - widthConnection) / 2, p1Y + (2 * radiusChip - widthConnection) / 2);
+                connectingPoints.setSize(size);
+                connectingPoints.setPosition(position);
             }
-
-            for (int i = 0; i < config.getConnectCount(); i++)
-            {
-                int p1 = config.getConnectionsBetweenPoints(i).getConnectionP1();
-                int p2 = config.getConnectionsBetweenPoints(i).getConnectionP2();
-                float p1X = config.getCoordinatePoints(p1).getCoordinateX();
-                float p1Y = config.getCoordinatePoints(p1).getCoordinateY();
-                float p2X = config.getCoordinatePoints(p2).getCoordinateX();
-                float p2Y = config.getCoordinatePoints(p2).getCoordinateY();
-                float widthConnection = 20;
-                sf::RectangleShape connectingPoints;
-
-                if (p1X == p2X)
-                {
-                    sf::Vector2f size(widthConnection, p2Y + sizePoints.y - p1Y - (sizePoints.y - widthConnection));
-                    sf::Vector2f position(p1X + ((sizePoints.x - widthConnection) / 2), p1Y + ((sizePoints.y - widthConnection) / 2));
-                    connectingPoints.setSize(size);
-                    connectingPoints.setPosition(position);
-                }
-                else
-                {
-                    sf::Vector2f size(p2X + sizePoints.x - p1X - (sizePoints.x - widthConnection), widthConnection);
-                    connectingPoints.setSize(size);
-                    sf::Vector2f position(p1X + ((sizePoints.x - widthConnection) / 2), p1Y + ((sizePoints.y - widthConnection) / 2));
-                    connectingPoints.setPosition(position);
-                }
+            else {
+                sf::Vector2f size(p2X + sizePoints.x - p1X - (sizePoints.x - widthConnection), widthConnection);
+                connectingPoints.setSize(size);
+                sf::Vector2f position(p1X + (2 * radiusChip - widthConnection) / 2, p1Y + (2 * radiusChip - widthConnection) / 2);
+                connectingPoints.setPosition(position);
+            }
 
                 connectingPoints.setFillColor(sf::Color(216, 216, 216));
                 window.draw(connectingPoints);
             }
 
-            for (int i = 0; i < square.size(); i++)
-            {
-                window.draw(square[i].point);
-            }
+        for (int i = 0; i < square.size(); i++) {
+            window.draw(square[i].point);
+        }
 
-            for (int i = 0; i < chip.size(); i++) {
+        for (size_t i = 0; i < road.size(); i++) {
+            sf::CircleShape activ;
+            int radiusFreePosition = radiusChip * 0.2;
+            activ.setFillColor(sf::Color::Red);
+            activ.setRadius(radiusFreePosition);
+            activ.setPosition(positionPoints[*(road[i].end() - 1) - 1].coordinateX + (2 * radiusChip - 2 * radiusFreePosition) / 2 , positionPoints[*(road[i].end() - 1) - 1].coordinateY + (2 * radiusChip - 2 * radiusFreePosition) / 2);
+            window.draw(activ);
+        }
+
+        for (size_t i = 0; i < chip.size(); i++) {
+        for (int i = 0; i < chip.size(); i++) {
 
                 if (chip[i].numberPositionShape == activChip) {
-
-                    for (size_t i = 0; i < road.size(); i++)
-                    {
-
-                        if (*(road[i].end() - 1) == activPosition)
-                        {
-                            roadActivChip = road[i];
-                            stepActivChip = 1;
-                            break;
-                        }
+                for (size_t i = 0; i < road.size(); i++) {
+                {
+                    if (*(road[i].end() - 1) == activPosition) {
+                        roadActivChip = road[i];
+                        stepActivChip = 1;
+                        break;
+                    }
+                    }
 
                     }
 
@@ -316,21 +290,31 @@ int main()
 
                 window.draw(chip[i].shape);
             }
-
-            for (size_t i = 0; i < chip.size(); i++) {
-                if (chip[i].numberPositionShape == chip[i].numberWinPOsitionShape)
-                {
-                    countWinPosition++;
-                }
+        for (size_t i = 0; i < chip.size(); i++) {
+            if (chip[i].numberPositionShape == chip[i].numberWinPOsitionShape)
+            {
+                countWinPosition++;
             }
         }
-        else
-        {
-            text.setPosition(80, 200);
-            window.clear(sf::Color::Black);
-            window.draw(text);
-        }
         
+        window.display();
+    }
+
+    if (!soundWin.openFromFile("music/finish.ogg")) {
+        return 0;
+    }
+
+    if (countWinPosition == chip.size()) {
+        soundWin.play();
+    }
+
+    while (soundWin.getStatus() == 2) {
+        text.setPosition(80, 200);
+        window.clear(sf::Color::Black);
+        window.draw(text);
+        text.setPosition(80, 200);
+        window.clear(sf::Color::Black);
+        window.draw(text);
         window.display();
     }
 
